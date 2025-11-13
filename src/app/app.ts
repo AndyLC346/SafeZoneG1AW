b9ad20c362a5d3c397d573cf4368632031eeaba9
Andy Jheremy Lazaro Curia <149219743+AndyLC346@users.noreply.github.com>
2025-11-12 21:47:07 -0500

    correciones de usuarios

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { Home } from "./components/home/home";
+import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Home],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  imports: [Home, RouterOutlet]
 })
 export class App {
   protected readonly title = signal('SafeZoneG1AW');
