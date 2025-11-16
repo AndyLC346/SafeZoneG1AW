412620a2a9820308057e7fef0fd4c3ed0896cb84
Sebas27101989 <u202315630@upc.edu.pe>
2025-11-16 04:14:26 -0500

    TicketReporteListar.ts

--- src/app/components/ticket-reporte/ticketreportelistar/ticketreportelistar.ts
+++ src/app/components/ticket-reporte/ticketreportelistar/ticketreportelistar.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TicketSoporteService } from '../../../services/ticket-soporte-service';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
+import { TicketSoporte } from '../../../models/TicketSoporte';
 
 @Component({
   selector: 'app-ticketreportelistar',
-  imports: [],
+  imports: [MatTableModule,MatButtonModule,MatIconModule,RouterLink],
   templateUrl: './ticketreportelistar.html',
   styleUrl: './ticketreportelistar.css',
 })
-export class Ticketreportelistar {
+export class Ticketreportelistar implements OnInit{
 
+  dataSource:MatTableDataSource<TicketSoporte>=new MatTableDataSource();
+
+  displayedColumns:string[]=['c1','c2','c3','c4','c5','c6','c7']
+  
+  constructor(private tS:TicketSoporteService){}
+
+  ngOnInit(): void {
+    this.tS.list().subscribe((data) => {
+      this.dataSource = new MatTableDataSource(data);
+    });
+    this.tS.getList().subscribe((data) => {
+      this.dataSource = new MatTableDataSource(data);
+    });
+  }
+
+  eliminar(id:number){
+    this.tS.delete(id).subscribe((data)=>{
+      this.tS.list().subscribe(data=>{
+        this.tS.setList(data)
+      })
+    });
+  }
 }
