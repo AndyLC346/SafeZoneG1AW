36b9f49a59c9857b7b97067704c699c35b66f661
Sebas27101989 <u202315630@upc.edu.pe>
2025-11-16 03:07:35 -0500

    TicketSoporte atributos

--- src/app/models/TicketSoporte.ts
+++ src/app/models/TicketSoporte.ts
@@ -1,3 +1,10 @@
-export class TicketSoporte{
+import { Users } from "./Users"
 
+export class TicketSoporte{
+    tipoSoporte:string=""
+    asuntoSoporte:string=""
+    descripcionSoporte:string=""
+    estadoSoporte:string=""
+    fechacreacionSoporte:Date=new Date()
+    users:Users=new Users()
 }
