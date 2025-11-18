ccbd0110de0274b75e5adbe3360d3a23c8090ea7
JohnnyM2005 <u202313751@upc.edu.pe>
2025-11-18 17:52:24 -0500

    Auditoria registrar avance

--- src/app/components/auditoria/auditoriaregistrar/auditoriaregistrar.ts
+++ src/app/components/auditoria/auditoriaregistrar/auditoriaregistrar.ts
@@ -1,101 +1,96 @@
 import { Component, OnInit } from '@angular/core';
 import {
   FormBuilder,
-  FormControl,
   FormGroup,
-  ReactiveFormsModule,
   Validators,
+  FormControl
 } from '@angular/forms';
 import { Auditoria } from '../../../models/Auditoria';
 import { AuditoriaService } from '../../../services/auditoria-service';
 import { ActivatedRoute, Params, Router } from '@angular/router';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { provideNativeDateAdapter } from '@angular/material/core';
-import { MatButtonModule } from '@angular/material/button';
 
 @Component({
-  selector: 'app-auditoria-registrar',
-  standalone: true,
-  imports: [
-    ReactiveFormsModule,
-    MatInputModule,
-    MatFormFieldModule,
-    MatRadioModule,
-    MatDatepickerModule,
-    MatButtonModule,
-  ],
+  selector: 'app-auditoriaregistrar',
   templateUrl: './auditoriaregistrar.html',
   styleUrls: ['./auditoriaregistrar.css'],
-  providers: [provideNativeDateAdapter()],
 })
-export class AuditoriaRegistrar implements OnInit {
-  form: FormGroup = new FormGroup({});
-  aud: Auditoria = new Auditoria();
+export class AuditoriaRegistrarComponent implements OnInit {
 
-  edicion: boolean = false;
-  id: number = 0;
+  form: FormGroup;
+  audit: Auditoria = new Auditoria();
+
+  edicion = false;
+  id = 0;
 
   constructor(
     private aS: AuditoriaService,
     private router: Router,
-    private formBuilder: FormBuilder,
+    private fb: FormBuilder,
     private route: ActivatedRoute
-  ) {}
+  ) {
+    // inicializamos el form aquí por seguridad
+    this.form = this.fb.group({
+      codigo: [''],
+      tipoAuditoria: ['', Validators.required],
+      descripcion: ['', Validators.required],
+      fechaAuditoria: ['', Validators.required],
+      usuario: ['', Validators.required], // ID del usuario
+    });
+  }
 
   ngOnInit(): void {
     this.route.params.subscribe((data: Params) => {
       this.id = data['id'];
       this.edicion = data['id'] != null;
-      this.init();
-    });
-
-    this.form = this.formBuilder.group({
-      fechaAuditoria: ['', Validators.required],
-      tipoAuditoria: ['', Validators.required],
-      descripcion: ['', Validators.required],
-      usuarioId: ['', Validators.required],  // Asumiendo que seleccionas usuario por ID
+      if (this.edicion) {
+        this.init();
+      }
     });
   }
 
   aceptar(): void {
-    if (this.form.valid) {
-      this.aud.idAuditoria = this.id;  // Si edición, ID ya viene
-      this.aud.fechaAuditoria = this.form.value.fechaAuditoria;
-      this.aud.tipoAuditoria = this.form.value.tipoAuditoria;
-      this.aud.descripcion = this.form.value.descripcion;
-      // usuario: asignar un objeto Users, aquí simplificado
-      this.aud.usuario = { id: this.form.value.usuarioId } as any;
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
 
-      if (this.edicion) {
-        this.aS.update(this.aud).subscribe(() => {
-          this.aS.list().subscribe((data) => {
-            this.aS.setList(data);
-          });
-        });
-      } else {
-        this.aS.insert(this.aud).subscribe(() => {
-          this.aS.list().subscribe((data) => {
-            this.aS.setList(data);
-          });
-        });
-      }
-      this.router.navigate(['auditorias']);
+    this.audit.idAuditoria = this.form.value.codigo;
+    this.audit.tipoAuditoria = this.form.value.tipoAuditoria;
+    this.audit.descripcion = this.form.value.descripcion;
+    this.audit.fechaAuditoria = this.form.value.fechaAuditoria;
+
+    // si tu modelo usuario tiene 'id' como propiedad
+    if (!this.audit.usuario) {
+      this.audit.usuario = { id: this.form.value.usuario } as any;
+    } else {
+      (this.audit.usuario as any).id = this.form.value.usuario;
+    }
+
+    if (this.edicion) {
+      this.aS.update(this.audit).subscribe(() => {
+        this.aS.list().subscribe((data) => this.aS.setList(data));
+        this.router.navigate(['auditorias']);
+      });
+    } else {
+      this.aS.insert(this.audit).subscribe(() => {
+        this.aS.list().subscribe((data) => this.aS.setList(data));
+        this.router.navigate(['auditorias']);
+      });
     }
   }
 
   init() {
-    if (this.edicion) {
-      this.aS.listId(this.id).subscribe((data) => {
-        this.form = new FormGroup({
-          fechaAuditoria: new FormControl(data.fechaAuditoria),
-          tipoAuditoria: new FormControl(data.tipoAuditoria),
-          descripcion: new FormControl(data.descripcion),
-          usuarioId: new FormControl(data.usuario?.id),
-        });
+    this.aS.listId(this.id).subscribe((data) => {
+     
+      this.form.patchValue({
+        codigo: data.idAuditoria,
+        tipoAuditoria: data.tipoAuditoria,
+        descripcion: data.descripcion,
+        fechaAuditoria: data.fechaAuditoria,
+        usuario: data.usuario?.id ?? '',
       });
-    }
+    }, err => {
+      console.error('Error al cargar auditoría:', err);
+    });
   }
 }
