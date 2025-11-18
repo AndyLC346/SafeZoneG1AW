5fd897ebead98c48af4a4614c4d558e1c842d9c5
Sebas27101989 <u202315630@upc.edu.pe>
2025-11-18 10:35:35 -0500

    RespuestaSoporteListar

---
+++ src/app/components/respuesta-soporte/respuestasoportelistar/respuestasoportelistar.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { RespuestaSoporte } from '../../../models/RespuestaSoporte';
+import { RespuestaSoporteService } from '../../../services/respuesta-soporte-service';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-respuestasoportelistar',
+  imports: [MatTableModule, MatButtonModule, MatIconModule, RouterLink,CommonModule],
+  templateUrl: './respuestasoportelistar.html',
+  styleUrl: './respuestasoportelistar.css',
+})
+export class Respuestasoportelistar implements OnInit {
+  dataSource:MatTableDataSource<RespuestaSoporte>=new MatTableDataSource();
+  displayedColumns: string[] = ['c1', 'c2', 'c3', 'cF', 'c4', 'c5'];
+  constructor(private rS:RespuestaSoporteService){}
+
+  ngOnInit(): void {
+    this.rS.list().subscribe((data) => {
+      this.dataSource = new MatTableDataSource(data);
+    });
+    this.rS.getList().subscribe((data) => {
+      this.dataSource = new MatTableDataSource(data);
+    });
+  }
+    
+  eliminar(id: number) {
+    this.rS.delete(id).subscribe((data) => {
+      this.rS.list().subscribe(data=>{
+        this.rS.setList(data)
+      })
+    });
+  }
+}
--- src/app/models/RespuestaSoporte.ts
+++ src/app/models/RespuestaSoporte.ts
@@ -1,3 +1,8 @@
-export class RespuestaSoporte{
+import { TicketSoporte } from "./TicketSoporte"
 
+export class RespuestaSoporte{
+    idRespuestaSoporte:number=0
+    mensajeRespuestaSoporte:string=""
+    fechacierreRespuestaSoporte:Date=new Date()
+    ticketreporte:TicketSoporte=new TicketSoporte()
 }
