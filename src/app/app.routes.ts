26d619093dbf5d8d4f256a8d1f82166d05d57d49
Sebas27101989 <u202315630@upc.edu.pe>
2025-11-18 13:25:03 -0500

    rutas y mejora

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,10 @@ import { Routes } from '@angular/router';
 import { Home } from './components/home/home';
 import { Users } from './components/users/users';
 import { Usuarioregistrar } from './components/users/usuarioregistrar/usuarioregistrar';
-import { TicketSoporte } from './models/TicketSoporte';
-import { Ticketreportelistar } from './components/ticket-reporte/ticketreportelistar/ticketreportelistar';
 import { Ticketreporteregistrar } from './components/ticket-reporte/ticketreporteregistrar/ticketreporteregistrar';
 import { TicketReporte } from './components/ticket-reporte/ticket-reporte';
+import { RespuestaSoporte } from './components/respuesta-soporte/respuesta-soporte';
+import { Respuestasoportelistar } from './components/respuesta-soporte/respuestasoportelistar/respuestasoportelistar';
 
 export const routes: Routes = [
     {
@@ -27,6 +27,14 @@ export const routes: Routes = [
         children: [
             { path: 'news', component: Ticketreporteregistrar },
             { path: 'edits/:id', component: Ticketreporteregistrar },
+        ],
+    },
+    {
+        path: 'listar',
+        component: RespuestaSoporte, 
+        children: [
+            { path: 'news', component: Respuestasoportelistar },
+            { path: 'edits/:id', component: Respuestasoportelistar },
         ],
     },
 ];
