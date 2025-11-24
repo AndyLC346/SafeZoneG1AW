611f3498105041a5002a4f2e5df2a57776183454
Andy Jheremy Lazaro Curia <u202013944@upc.edu.pe>
2025-11-23 22:37:17 -0500

    creacion de archivo

---
+++ src/app/components/archivos/archivolistar/archivolistar.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-archivolistar',
+  imports: [],
+  templateUrl: './archivolistar.html',
+  styleUrl: './archivolistar.css',
+})
+export class Archivolistar {
+
+}
