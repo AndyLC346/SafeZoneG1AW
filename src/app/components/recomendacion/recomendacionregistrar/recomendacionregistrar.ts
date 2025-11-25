6beb7ed265a6fd936b0c2e0f7aea1a9ecf8469e5
Andy Jheremy Lazaro Curia <149219743+AndyLC346@users.noreply.github.com>
2025-11-25 09:55:41 -0500

    creacion de recomendaciones

---
+++ src/app/components/recomendacion/recomendacionregistrar/recomendacionregistrar.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recomendacionregistrar',
+  imports: [],
+  templateUrl: './recomendacionregistrar.html',
+  styleUrl: './recomendacionregistrar.css',
+})
+export class Recomendacionregistrar {
+
+}
