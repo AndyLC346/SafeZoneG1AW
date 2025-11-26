0430380e1d936e1fd5e9effff93c0dec8ec3f637
AngelSoria1 <u202316237@upc.edu.pe>
2025-11-25 21:15:41 -0500

    SECURITY IMPLEMENT FRONTEND

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,12 +13,29 @@ import { Inicio } from './components/inicio/inicio';
 import { Registrar } from './components/registrar/registrar';
 import { Login } from './components/login/login';
 import { BuscarArchivosXFecha } from './components/archivos/buscar-archivos-xfecha/buscar-archivos-xfecha';
+import { Alerta } from './components/alerta/alerta';
+import { Recurso } from './components/recurso/recurso';
+import { Role } from './components/role/role';
+import { APIcomponent } from './components/apicomponent/apicomponent';
+import { Rollistar } from './components/role/rollistar/rollistar';
+import { Rolregistrar } from './components/role/rolregistrar/rolregistrar';
+import { Recursobuscar } from './components/recurso/recursobuscar/recursobuscar';
+import { Recursolistar } from './components/recurso/recursolistar/recursolistar';
+import { Recursoregistrar } from './components/recurso/recursoregistrar/recursoregistrar';
+import { Recursoquery1 } from './components/recurso/recursoquery1/recursoquery1';
+import { Recursoquery2 } from './components/recurso/recursoquery2/recursoquery2';
+import { Alertalistar } from './components/alerta/alertalistar/alertalistar';
+import { Alertaregistrar } from './components/alerta/alertaregistrar/alertaregistrar';
+import { Alertabuscar } from './components/alerta/alertabuscar/alertabuscar';
+import { Alertaquery1 } from './components/alerta/alertaquery1/alertaquery1';
+import { Autenticador } from './components/autenticador/autenticador';
+import { seguridadGuardGuard } from './guard/seguridad-guard-guard';
 
 
 export const routes: Routes = [
     {
-        path: '', //ruta por default
-        redirectTo: 'inicio',
+        path: '',
+        redirectTo: 'login',
         pathMatch: 'full',
     },
     {
@@ -31,11 +48,12 @@ export const routes: Routes = [
     },
     {
     path: 'login',
-    component: Login,
+    component: Autenticador,
     },
     {
         path: 'home',
         component: Home,
+         canActivate: [seguridadGuardGuard],
     },
     {
         path: 'menu',
@@ -75,5 +93,48 @@ export const routes: Routes = [
             { path: 'registrar', component: Recomendacionregistrar },
             { path: 'actualizar/:id', component: Recomendacionregistrar },
         ]
-    }
+    },
+
+     {
+        path: 'alertas',
+        component: Alerta,
+        children: [
+            { path: 'listar', component: Alertalistar },
+            { path: 'registrar', component: Alertaregistrar },
+            { path: 'editar/:id', component: Alertaregistrar },
+            { path: 'buscar', component: Alertabuscar },
+             {path: 'reporte1',component: Alertaquery1,},
+        ],
+    },
+     {
+        path: 'recursosducativos',
+        component: Recurso,
+        children: [
+            { path: 'listar', component: Recursolistar },
+            { path: 'registrar', component: Recursoregistrar }, 
+            { path: 'editar/:id', component: Recursoregistrar },
+            { path: 'buscar', component: Recursobuscar },
+            {path: 'reporte1',component: Recursoquery1,},
+            {path: 'reporte2',component: Recursoquery2,},
+           
+        ],
+    },
+
+    {
+        path: 'roles',
+        component: Role,
+        children: [
+            { path: 'listar', component: Rollistar },
+            { path: 'registrar', component: Rolregistrar }, 
+            { path: 'editar/:id', component: Rolregistrar },
+            { path: 'buscar', component: Recursobuscar },
+           
+        ],
+    },
+
+      {
+        path: 'scan',
+        component: APIcomponent,
+    },
+
 ];
