6835ef5fe4a66cf4ddc8da2851d90bfcafc582d1
AngelSoria1 <u202316237@upc.edu.pe>
2025-11-30 12:34:56 -0500

    TICKET

--- src/app/models/TicketSoporte.ts
+++ src/app/models/TicketSoporte.ts
@@ -1,3 +1,11 @@
-export class TicketSoporte{
+import { Users } from "./Users"
 
+export class TicketSoporte{
+    idSoporte:number=0
+    tipoSoporte:string=""
+    asuntoSoporte:string=""
+    descripcionSoporte:string=""
+    estadoSoporte:string=""
+    fechacreacionSoporte:Date=new Date()
+    usuario:Users=new Users()
 }
