22ef7b5c7c0b21e658f734af9bd50e51d913d511
JohnnyM2005 <u202313751@upc.edu.pe>
2025-11-30 21:33:18 -0500

    log acceso registrar actualizado

--- src/app/components/log-acceso/log-accesoregistrar/log-accesoregistrar.ts
+++ src/app/components/log-acceso/log-accesoregistrar/log-accesoregistrar.ts
@@ -1,33 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {
-  FormBuilder,
-  FormControl,
-  FormGroup,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
-import { LogAcceso } from '../../../models/LogAcceso';
-
-import { LogAccesoService } from '../../../services/log-acceso-service';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ActivatedRoute, Params, Router, RouterLink } from '@angular/router';
+
+
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { provideNativeDateAdapter } from '@angular/material/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+
+
+import { LogAcceso } from '../../../models/LogAcceso';
+import { LogAccesoService } from '../../../services/log-acceso-service';
+import { Users } from '../../../models/Users';
+import { UsersService } from '../../../services/users-service';
 
 @Component({
   selector: 'app-logaccesoregistrar',
   standalone: true,
   imports: [
-    CommonModule,
-    ReactiveFormsModule,
-    MatInputModule,
-    MatFormFieldModule,
-    MatDatepickerModule,
-    MatButtonModule,
-    RouterLink
+    CommonModule, ReactiveFormsModule, MatInputModule, MatFormFieldModule,
+    MatDatepickerModule, MatButtonModule, RouterLink, MatSelectModule
   ],
   templateUrl: './log-accesoregistrar.html',
   styleUrls: ['./log-accesoregistrar.css'],
@@ -39,19 +34,17 @@ export class LogAccesoRegistrar implements OnInit {
   edicion: boolean = false;
   id: number = 0;
 
+  listaUsuarios: Users[] = [];
+
   constructor(
     private lS: LogAccesoService,
+    private uS: UsersService,
     private router: Router,
     private formBuilder: FormBuilder,
     private route: ActivatedRoute
   ) {}
 
   ngOnInit(): void {
-    this.route.params.subscribe((data: Params) => {
-      this.id = data['id'];
-      this.edicion = data['id'] != null;
-      this.init();
-    });
 
     this.form = this.formBuilder.group({
       codigo: [''],
@@ -61,49 +54,58 @@ export class LogAccesoRegistrar implements OnInit {
       sistemaoperativoAcceso: ['', Validators.required],
       usuario: ['', Validators.required],
     });
+
+
+    this.uS.list().subscribe(data => {
+      this.listaUsuarios = data;
+
+
+      this.route.params.subscribe((params: Params) => {
+        this.id = params['id'];
+        this.edicion = params['id'] != null;
+
+        if (this.edicion) {
+          this.init();
+        }
+      });
+    });
   }
 
   aceptar(): void {
     if (this.form.valid) {
       this.log.idLogAcceso = this.edicion ? this.id : 0;
-
       this.log.fechaAcceso = this.form.value.fechaAcceso;
       this.log.ipAcceso = this.form.value.ipAcceso;
       this.log.navegadorAcceso = this.form.value.navegadorAcceso;
       this.log.sistemaoperativoAcceso = this.form.value.sistemaoperativoAcceso;
 
-      this.log.usuario = { id: this.form.value.usuario } as any;
+      this.log.usuario = new Users();
+      this.log.usuario.id = this.form.value.usuario;
 
       if (this.edicion) {
         this.lS.update(this.log).subscribe(() => {
-          this.lS.list().subscribe((data) => {
-            this.lS.setList(data);
-          });
-          this.router.navigate(['logacceso']);
+          this.lS.list().subscribe(data => this.lS.setList(data));
+          this.router.navigate(['/logacceso/listar']); // Redirige a listar
         });
       } else {
         this.lS.insert(this.log).subscribe(() => {
-          this.lS.list().subscribe((data) => {
-            this.lS.setList(data);
-          });
-          this.router.navigate(['logacceso']);
+          this.lS.list().subscribe(data => this.lS.setList(data));
+          this.router.navigate(['/logacceso/listar']); // Redirige a listar
         });
       }
     }
   }
 
   init() {
-    if (this.edicion) {
-      this.lS.listId(this.id).subscribe((data) => {
-        this.form.patchValue({
-          codigo: data.idLogAcceso,
-          fechaAcceso: new Date(data.fechaAcceso),
-          ipAcceso: data.ipAcceso,
-          navegadorAcceso: data.navegadorAcceso,
-          sistemaoperativoAcceso: data.sistemaoperativoAcceso,
-          usuario: data.usuario.id,
-        });
+    this.lS.listId(this.id).subscribe((data) => {
+      this.form.patchValue({
+        codigo: data.idLogAcceso,
+        fechaAcceso: new Date(data.fechaAcceso),
+        ipAcceso: data.ipAcceso,
+        navegadorAcceso: data.navegadorAcceso,
+        sistemaoperativoAcceso: data.sistemaoperativoAcceso,
+        usuario: data.usuario.id, 
       });
-    }
+    });
   }
 }
